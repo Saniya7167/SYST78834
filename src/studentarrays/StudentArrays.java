bab4896e304b5018be3f6103a79e079c29ad8905
saniy <saniy@DESKTOP-QBDDNEN>
2022-01-27 02:39:37 -0500

    Checkpoint 1 - added the introduction to the toc and began writing the introduction part

---
+++ src/studentarrays/StudentArrays.java
@@ -0,0 +1,23 @@
+package studentarrays;
+import java.util.Scanner;
+/**
+ *
+ * @author Saniya
+ */
+public class StudentArrays
+{
+   /**
+    * @param args the command line arguments
+    */
+   public static void main (String[] args)
+   {
+      Student s1 = new Student();
+      Scanner input = new Scanner(System.in);
+      System.out.println("Enter Student Name:   "); // Taking user input
+      s1.setName(input.nextLine());
+
+      System.out.println("name:    " + s1.getName());
+
+      //Student []  studlist = new Student[5];// array of object
+   }
+}
